a53badfeb43a5970997c4fdedaa9372e7b04d705
cprapal2005 <cprapal2005@g.educaand.es>
2024-01-16 18:58:07 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Aplicacion_Cursos',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
---
+++ src/app/formulario/formulario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'formulario',
+  templateUrl: './formulario.component.html',
+  styleUrls: ['./formulario.component.scss'],
+})
+export class FormularioComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
