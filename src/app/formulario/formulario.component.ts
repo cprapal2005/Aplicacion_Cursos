fe9ce5d6832bd799b1289a72060c6fa636daa4d6
cprapal2005 <cprapal2005@g.educaand.es>
2024-01-18 19:25:45 +0100

    Act

--- src/app/formulario/formulario.component.ts
+++ src/app/formulario/formulario.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'formulario',
@@ -7,8 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent  implements OnInit {
 
-  constructor() { }
+  @Output() nombreCurso = new EventEmitter<string>();
+  @Output() puntosCurso = new EventEmitter<number>();
+  cursoForm!: FormGroup;
 
-  ngOnInit() {}
+  constructor(private formBuilder: FormBuilder) {}
+
+  añadirCurso(curso: string, puntos: number) {
+
+    this.nombreCurso.emit(curso);
+    this.puntosCurso.emit(puntos);
+
+  }
+  
+  ngOnInit() {
+    this.cursoForm = this.formBuilder.group({
+      curso: ['', [Validators.required]],
+      puntos: ['', [Validators.required, Validators.pattern("^[0-9]")]]
+    });
+  }
+
+  onSubmit() {
+    if (this.cursoForm.valid) {
+      this.añadirCurso(this.cursoForm.value.curso, this.cursoForm.value.puntos);
+    }
+  }
 
 }
